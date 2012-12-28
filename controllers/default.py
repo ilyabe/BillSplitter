675d2c80e37503d372ada00c64df1dea58fe0572
Ilya Bernshteyn <ilyabe@gmail.com>
2012-12-27 21:04:35 -0600

    First commit

---
+++ controllers/default.py
@@ -0,0 +1,243 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+#########################################################################
+## This is a samples controller
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+## - call exposes all registered services (none by default)
+#########################################################################
+
+def index():
+    """
+    example action using the internationalization operator T and flash
+    rendered by views/default/index.html or views/generic.html
+
+    if you need a simple wiki simple replace the two lines below with:
+    return auth.wiki()
+    """
+    response.flash = T("Welcome to web2py!")
+    return dict(message=T('Welcome to Bill Splitter'))
+
+# Users can create homes
+@auth.requires_login()
+def create_home():
+    # User creates the home
+    form = SQLFORM(db.home).process()
+
+    # Add record in user_home for creating user
+    # Creating user automatically becomes the admin
+    if form.accepted:
+        db.user_home.insert(user=auth.user_id, home=form.vars.id, is_admin=True, status='active')
+        redirect(URL('my_homes'))
+
+    return locals()
+
+# Users can view a list of their homes
+@auth.requires_login()
+def my_homes():
+    homes = db(db.user_home.user==auth.user_id).select(db.user_home.ALL)
+    
+    my_homes = []
+    for home in homes:
+        my_home = {'id': home.home, 'name': db(db.home.id==home.home).select(db.home.home_name).first().home_name, 'status': home.status, 'is_admin':home.is_admin}
+        my_homes.append(my_home)
+
+    return locals()
+
+# Users can add bills
+@auth.requires_login()
+def add_bill():
+    # If the user is not the admin of this home, redirect to my_homes
+    # Get this home's admin
+    admin_id = db((db.user_home.home==request.args(0)) & (db.user_home.is_admin==True)).select(db.user_home.user).first()['user']
+
+    # If auth.user ID doesn't match, redirect
+    if auth.user_id != admin_id:
+        redirect(URL('my_homes'))
+
+    # Else allow adding the bill for this home
+    db.bill.home.default = request.args(0)
+    db.bill.created_on.default = request.now
+    form = SQLFORM(db.bill, fields=['company_name', 'amount', 'bill', 'created_on']).process()
+
+    return locals()
+
+# Users can view bills for a specific home
+@auth.requires_login()
+def view_bills():
+    # User's status must be active, else redirect
+    user_status = db((db.user_home.home==request.args(0)) & (db.user_home.user==auth.user_id)).select(db.user_home.status).first()['status']
+    if user_status != 'active':
+        redirect(URL('my_homes'))
+
+    # All bills for this home
+    home_bills = db(db.bill.home==request.args(0)).select(db.bill.ALL)
+
+    # Total all time bill
+    total_bill = 0
+    for bill in home_bills:
+        #if (bill.created_on.year == request.now.year) & (bill.created_on.month == request.now.month):
+        total_bill += bill.amount
+
+    # Each roommate's portion is calculated as the total all time bill divided by total roommates minus the sum of this roommate's payments
+    roommate_bill = 0
+    roommate_count = db((db.user_home.home==request.args(0))).count() # Number of roommates in this home
+
+    # Check if this user has made any payments
+    this_roommate_payments = 0 # total of this roommate's payments, if any
+    user_count = 0
+    user_count = db((db.payments.user==auth.user_id)&(db.payments.home==request.args(0))).count()
+
+    this_roommates_payments = 0
+    if user_count != 0: # If the user has made payments, get the current sum
+        this_roommates_payments = db((db.payments.user==auth.user_id)&(db.payments.home==request.args(0))).select(db.payments.total_payments).first()['total_payments']
+
+    if roommate_count > 0:
+        roommate_bill = round((total_bill / roommate_count) - this_roommates_payments, 2)
+
+    # If the user viewing the page is the home's admin, is_admin_user = True
+    admin_id = db((db.user_home.home==request.args(0)) & (db.user_home.is_admin==True)).select(db.user_home.user).first()['user']
+
+    is_admin_user = False
+    if auth.user_id == admin_id:
+        is_admin_user = True
+
+    return locals()
+
+# Users can make a request to join a home and view its bills by providing the home's ID
+@auth.requires_login()
+def join_home():
+    db.user_home.user.default = auth.user_id
+    db.user_home.is_admin.default = False
+    db.user_home.status.default = 'pending'
+    form = SQLFORM(db.user_home, fields=['home']).process()
+    return locals()
+
+# A home's admin can approve/deny requests to join
+@auth.requires_login()
+def review():
+    # Get a list of IDs of all homes where I am the admin
+    admin_homes = db((db.user_home.user==auth.user_id) & (db.user_home.is_admin==True)).select(db.user_home.ALL).as_list()
+
+    # For each of the homes where I am the admin, get a list of pending requests
+    my_requests = []
+    for home in admin_homes:
+        pending = db((db.user_home.home==home['home']) & (db.user_home.status=='pending')).select(db.user_home.ALL).as_list()
+        my_requests.append(pending)
+
+    return locals()
+
+# Called when the user clicks the Approve button on the review page
+@auth.requires_login()
+def approve():
+    db(db.user_home.id==request.args(0)).update(status='active')
+
+# Called when the user clicks the Deny button on the review page
+@auth.requires_login()
+def deny():
+    db(db.user_home.id==request.args(0)).delete()
+
+# Users can enter their credit card information to pay a particular month's bill
+@auth.requires_login()
+def pay_bill():
+    return locals()
+
+# Called when the submits their credit card information
+def charge():
+    print 'called charged2'
+
+    roommate_bill_in_cents = 0
+    roommate_bill_in_cents = str(request.args(0)).replace('.', '')
+
+    from gluon.contrib.stripe import Stripe
+    stripe = Stripe('sk_test_AOSmakDZoUJJ2UZD9keIrlLS')
+    d = stripe.charge(amount=roommate_bill_in_cents,
+            currency='usd',
+                  card_number=request.vars['card-number'],
+                  card_exp_month=request.vars['card-expiry-month'],
+                  card_exp_year=request.vars['card-expiry-year'],
+                  card_cvc_check=request.vars['card-cvc'],
+                  description='Bill Splitter')
+    paid = False
+    if d.get('paid',False):
+        # payment accepted
+        paid = True
+    else:
+        # error is in d.get('error','unknown')
+        error = d.get('error','unknown')
+        paid = False
+
+    # Update or insert the user into the payments table
+    if paid:    # If payment successful
+        user_count = 0
+        user_count = db((db.payments.user==auth.user_id)&(db.payments.home==request.args(1))).count()
+        if user_count == 0: # If the user is not in the payments table, insert them
+            db.payments.insert(user=auth.user_id, home=request.args(1), total_payments=request.args(0))
+        else: # The user is in the payments table, update the total amount they have paid
+            # Get the current total
+            current_total = 0
+            current_total = db((db.payments.user==auth.user_id)&(db.payments.home==request.args(1))).select(db.payments.total_payments).first()['total_payments']
+
+            # Sum the current total with the payment
+            from decimal import Decimal
+            new_total = 0
+            new_total = current_total + Decimal(request.args(0))
+
+            # Update payments record
+            db((db.payments.user==auth.user_id)&(db.payments.home==request.args(1))).update(total_payments=new_total)
+
+    return locals()
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
+
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request,db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
+
+
+@auth.requires_signature()
+def data():
+    """
+    http://..../[app]/default/data/tables
+    http://..../[app]/default/data/create/[table]
+    http://..../[app]/default/data/read/[table]/[id]
+    http://..../[app]/default/data/update/[table]/[id]
+    http://..../[app]/default/data/delete/[table]/[id]
+    http://..../[app]/default/data/select/[table]
+    http://..../[app]/default/data/search/[table]
+    but URLs must be signed, i.e. linked with
+      A('table',_href=URL('data/tables',user_signature=True))
+    or with the signed load operator
+      LOAD('default','data.load',args='tables',ajax=True,user_signature=True)
+    """
+    return dict(form=crud())
---
+++ models/db.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+
+#########################################################################
+## This scaffolding model makes your app work on Google App Engine too
+## File is released under public domain and you can use without limitations
+#########################################################################
+
+## if SSL/HTTPS is properly configured and you want all HTTP requests to
+## be redirected to HTTPS, uncomment the line below:
+# request.requires_https()
+
+if not request.env.web2py_runtime_gae:
+    ## if NOT running on Google App Engine use SQLite or other DB
+    db = DAL('sqlite://storage.sqlite')    
+else:
+    ## connect to Google BigTable (optional 'google:datastore://namespace')
+    db = DAL('google:datastore')
+    ## store sessions and tickets there
+    session.connect(request, response, db = db)
+    ## or store session in Memcache, Redis, etc.
+    ## from gluon.contrib.memdb import MEMDB
+    ## from google.appengine.api.memcache import Client
+    ## session.connect(request, response, db = MEMDB(Client()))
+
+## by default give a view/generic.extension to all actions from localhost
+## none otherwise. a pattern can be 'controller/function.extension'
+response.generic_patterns = ['*'] if request.is_local else []
+## (optional) optimize handling of static files
+# response.optimize_css = 'concat,minify,inline'
+# response.optimize_js = 'concat,minify,inline'
+
+#########################################################################
+## Here is sample code if you need for
+## - email capabilities
+## - authentication (registration, login, logout, ... )
+## - authorization (role based authorization)
+## - services (xml, csv, json, xmlrpc, jsonrpc, amf, rss)
+## - old style crud actions
+## (more options discussed in gluon/tools.py)
+#########################################################################
+
+from gluon.tools import Auth, Crud, Service, PluginManager, prettydate
+auth = Auth(db)
+crud, service, plugins = Crud(db), Service(), PluginManager()
+
+# Extra fields for user
+#auth.settings.extra_fields['auth_user']= [
+#  Field('is_admin', 'boolean', default=False, label='I will be the one paying the bills and collecting')]
+
+## create all tables needed by auth if not custom tables
+auth.define_tables(username=False, signature=False)
+
+## configure email
+mail=auth.settings.mailer
+mail.settings.server = 'logging' or 'smtp.gmail.com:587'
+mail.settings.sender = 'you@gmail.com'
+mail.settings.login = 'username:password'
+
+## configure auth policy
+auth.settings.registration_requires_verification = False
+auth.settings.registration_requires_approval = False
+auth.settings.reset_password_requires_verification = True
+
+## if you need to use OpenID, Facebook, MySpace, Twitter, Linkedin, etc.
+## register with janrain.com, write your domain:api_key in private/janrain.key
+from gluon.contrib.login_methods.rpx_account import use_janrain
+use_janrain(auth,filename='private/janrain.key')
+
+#########################################################################
+## Define your tables below (or better in another model file) for example
+##
+## >>> db.define_table('mytable',Field('myfield','string'))
+##
+## Fields can be 'string','text','password','integer','double','boolean'
+##       'date','time','datetime','blob','upload', 'reference TABLENAME'
+## There is an implicit 'id integer autoincrement' field
+## Consult manual for more options, validators, etc.
+##
+## More API examples for controllers:
+##
+## >>> db.mytable.insert(myfield='value')
+## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
+## >>> for row in rows: print row.id, row.myfield
+#########################################################################
+
+## after defining tables, uncomment below to enable auditing
+# auth.enable_record_versioning(db)
+
+# A table to store home names
+db.define_table('home',
+    Field('home_name', 'string', requires=[IS_NOT_EMPTY(), IS_NOT_IN_DB(db, 'home.home_name')]))
+
+# A table to store bills
+db.define_table('bill',
+    Field('company_name', 'string', requires=IS_NOT_EMPTY()),
+    Field('amount', 'decimal(5, 2)', requires=IS_NOT_EMPTY()),
+    Field('bill', 'upload'),
+    Field('home', 'reference home', writable=False),
+    Field('created_on', 'date', 'default=request.now'))
+
+# A table to store user-home pairs. i.e. the homes where the user is a roommate and whether they are admin
+db.define_table('user_home',
+    Field('user', 'reference auth_user'),
+    Field('home', 'reference home', requires=IS_IN_DB(db, 'home.id'), label='Home ID'),
+    Field('is_admin', 'boolean', writable=False),
+    Field('status', 'string', writable=False),
+    Field('amount_paid', 'decimal(10,2)'))
+
+# A table to store the total of a user's payments for a particular home
+db.define_table('payments',
+    Field('user', 'reference auth_user'),
+    Field('home', 'reference home'),
+    Field('total_payments', 'decimal(10,2)'))
+
+# global variables
+def name_of_home(home_id): return db(db.home.id==home_id).select(db.home.home_name)[0]['home_name']
+def name_of_user(user_id): return db(db.auth_user.id==user_id).select(db.auth_user.ALL)[0]['first_name'] + ' ' + db(db.auth_user.id==user_id).select(db.auth_user.ALL)[0]['last_name']
+def name_of_month(month):
+    month_name = ''
+    if month == 1:
+        month_name = 'January'
+    elif month == 2:
+        month_name = 'February'
+    elif month == 3:
+        month_name = 'March'
+    elif month == 4:
+        month_name = 'April'
+    elif month == 5:
+        month_name = 'May'
+    elif month == 6:
+        month_name = 'June'
+    elif month == 7:
+        month_name = 'July'
+    elif month == 8:
+        month_name = 'August'
+    elif month == 9:
+        month_name = 'September'
+    elif month == 10:
+        month_name = 'October'
+    elif month == 11:
+        month_name = 'November'
+    elif month == 12:
+        month_name = 'December'
+    return month_name
